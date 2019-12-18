6f35d02a7ec83d16df829bc10b3216f35aceaace
JasonQiu21 <51414606+JasonQiu21@users.noreply.github.com>
2019-12-18 12:42:22 -0500

    added health bars

--- src/Driver/Driver/PlayerTracker.py
+++ src/Driver/Driver/PlayerTracker.py
@@ -4,11 +4,12 @@ class PlayerTracker():
 
     def __init__(self):
         self.players = []
-        self.players.append(Player(xPos = 50, yPos = 50, strength = 5, speed = 100, hp = 250, hpRegen = 1.5, atk = 10, armor = 3, atkRange = 100, alliance = "a", visionRange = 500))
-        self.players.append(Player(xPos = 1000, yPos = 1000, strength = 5, speed = 100, hp = 250, hpRegen = 1.5, atk = 10, armor = 3, atkRange = 100, alliance = "b", visionRange = 500))        
+        self.players.append(Player(xPos = 50, yPos = 50, strength = 5, speed = 100, hp = 100, hpMax = 250, hpRegen = 1.5, atk = 10, armor = 3, atkRange = 100, alliance = "a", visionRange = 500))
+        self.players.append(Player(xPos = 1000, yPos = 1000, strength = 5, speed = 100, hp = 250, hpMax = 250, hpRegen = 1.5, atk = 10, armor = 3, atkRange = 100, alliance = "b", visionRange = 500))        
         
     def drawPlayers(self, Alliance):
         scale = 10000 / Alliance.resolution
         for i in self.players:
             if(Alliance.vision[i.x / scale][i.y / scale]):
-                i.drawPlayer();
+                i.drawPlayer()
+                i.drawHealth()
