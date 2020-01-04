9fd539fddb84445256145e595fc7062ff906b8a7
JasonQiu21 <51414606+JasonQiu21@users.noreply.github.com>
2020-01-04 18:31:08 -0500

    all towers added

--- src/Driver/Driver/StructureTracker.py
+++ src/Driver/Driver/StructureTracker.py
@@ -4,7 +4,41 @@ class StructureTracker():
 
     def __init__(self):
         self.structures = []
-        self.structures.append(Tower(xPos = 900, yPos = 900, alliance = "b"))
+        #nexus
+        self.structures.append(Tower(xPos = 300, yPos = 4650, alliance = "a"))
+        self.structures.append(Tower(xPos = 500, yPos = 4750, alliance = "a"))
+        #top
+        self.structures.append(Tower(xPos = 350, yPos = 3500, alliance = "a"))
+        self.structures.append(Tower(xPos = 350 , yPos = 2600, alliance = "a"))
+        self.structures.append(Tower(xPos = 350 , yPos = 1900, alliance = "a"))
+        #bot
+        self.structures.append(Tower(xPos = 1500, yPos = 4500, alliance = "a"))
+        self.structures.append(Tower(xPos = 2500, yPos = 4500, alliance = "a"))
+        self.structures.append(Tower(xPos = 3600, yPos = 4500, alliance = "a"))
+        #mid
+        self.structures.append(Tower(xPos = 1000, yPos = 4000, alliance = "a"))
+        self.structures.append(Tower(xPos = 1500, yPos = 3500, alliance = "a"))
+        self.structures.append(Tower(xPos = 2000, yPos = 3000, alliance = "a"))
+        
+        #nexus
+        self.structures.append(Tower(xPos = 4700, yPos = 500, alliance = "b"))
+        self.structures.append(Tower(xPos = 4450, yPos = 250, alliance = "b"))
+        #top
+        self.structures.append(Tower(xPos = 3700, yPos = 400, alliance = "b"))
+        self.structures.append(Tower(xPos = 2700, yPos = 400, alliance = "b"))
+        self.structures.append(Tower(xPos = 1700, yPos = 400, alliance = "b"))
+        #bot
+        self.structures.append(Tower(xPos = 4500, yPos = 1200, alliance = "b"))
+        self.structures.append(Tower(xPos = 4500, yPos = 2200, alliance = "b"))
+        self.structures.append(Tower(xPos = 4500, yPos = 3200, alliance = "b"))
+        #mid
+        self.structures.append(Tower(xPos = 3900, yPos = 1000, alliance = "b"))
+        self.structures.append(Tower(xPos = 3400, yPos = 1500, alliance = "b"))
+        self.structures.append(Tower(xPos = 2900, yPos = 2000, alliance = "b"))
+
+
+
+
         
     def drawStructures(self):
         for i in self.structures:
