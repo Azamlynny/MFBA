6a8ec8c3d8f26918555624c520c205525f72dea7
azamlynny <azamlynny@hotmail.com>
2020-01-27 11:50:49 -0500

    Balancing changes

--- src/Server/Driver/PlayerTracker.py
+++ src/Server/Driver/PlayerTracker.py
@@ -10,7 +10,7 @@ class PlayerTracker():
         self.players = []
         
         # Team A
-        self.players.append(Player(xPos = 100, yPos = 4500, wd = 60, ht = 60, strength = 7, speed = 7, hp = 250, hpMax = 250, hpRegen = 2, atk = 25, atkSpeed = 3, armor = 3, atkRange = 400, alliance = "a", visionRange = 750, atkType = "ranged"))
+        self.players.append(Player(xPos = 100, yPos = 4500, wd = 60, ht = 60, strength = 7, speed = 7, hp = 250, hpMax = 250, hpRegen = 2, atk = 15, atkSpeed = 3, armor = 3, atkRange = 400, alliance = "a", visionRange = 750, atkType = "ranged"))
         self.players.append(Player(xPos = 200, yPos = 4600, wd = 80, ht = 80, strength = 5, speed = 6, hp = 500, hpMax = 500, hpRegen = 4, atk = 35, atkSpeed = 1.0, armor = 5, atkRange = 100, alliance = "a", visionRange = 600, atkType = "melee"))     
         self.players.append(Player(xPos = 300, yPos = 4700, wd = 60, ht = 60, strength = 7, speed = 8, hp = 350, hpMax = 350, hpRegen = 2, atk = 40, atkSpeed = 2, armor = 3, atkRange = 125, alliance = "a", visionRange = 600, atkType = "melee"))
         self.players.append(Player(xPos = 400, yPos = 4800, wd = 70, ht = 70, strength = 5, speed = 6, hp = 350, hpMax = 350, hpRegen = 4, atk = 35, atkSpeed = 1.5, armor = 3, atkRange = 100, alliance = "a", visionRange = 500, atkType = "melee"))        
